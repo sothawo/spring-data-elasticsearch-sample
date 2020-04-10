d16b06fdc8ae4956258aaf43340b87d997922b7b
P.J. Meisch <pj.meisch@sothawo.com>
2020-04-10 15:54:13 +0200

    initial setup

---
+++ src/main/java/com/sothawo/springdataelasticsearchsample/SpringDataElasticsearchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sothawo.springdataelasticsearchsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticsearchSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataElasticsearchSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sothawo/springdataelasticsearchsample/SpringDataElasticsearchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sothawo.springdataelasticsearchsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataElasticsearchSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
