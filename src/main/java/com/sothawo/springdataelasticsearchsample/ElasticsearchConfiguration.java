6160d9139e6632bdf1832521ac19020b0f5bc797
P.J. Meisch <pj.meisch@sothawo.com>
2020-04-11 07:10:50 +0200

    transport configuration

--- src/main/java/com/sothawo/springdataelasticsearchsample/ElasticsearchConfiguration.java
+++ src/main/java/com/sothawo/springdataelasticsearchsample/ElasticsearchConfiguration.java
@@ -15,32 +15,35 @@
 */
 package com.sothawo.springdataelasticsearchsample;
 
-import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.config.ElasticsearchConfigurationSupport;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 
-import java.time.Duration;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author P.J. Meisch (pj.meisch@sothawo.com)
  */
 @Configuration
-public class ElasticsearchConfiguration extends AbstractElasticsearchConfiguration {
+public class ElasticsearchConfiguration extends ElasticsearchConfigurationSupport {
 
-    @Override
     @Bean
-    public RestHighLevelClient elasticsearchClient() {
+    public Client elasticsearchClient() throws UnknownHostException {
+        Settings settings = Settings.builder().put("cluster.name", "elasticsearch").build();
+        TransportClient client = new PreBuiltTransportClient(settings);
+        client.addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+        return client;
+    }
 
-        ClientConfiguration clientConfiguration = ClientConfiguration.builder() //
-            .connectedTo("localhost:9200") //
-            // .withProxy("localhost:8080")
-            // .withBasicAuth("elastic", "somePassword") //
-            .withSocketTimeout(Duration.ofSeconds(60)) //
-            .build();
-
-        return RestClients.create(clientConfiguration).rest();
+    @Bean(name = {"elasticsearchOperations", "elasticsearchTemplate"})
+    public ElasticsearchTemplate elasticsearchTemplate() throws UnknownHostException {
+        return new ElasticsearchTemplate(elasticsearchClient());
     }
 }
